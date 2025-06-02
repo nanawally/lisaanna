3ecbd995456d5cc04e56958aa4020ede0eab1ff6
Anna Wallström <anoli89@hotmail.com>
2025-06-02 14:26:24 +0200

    Initial commit

---
+++ src/main/java/org/example/lisaanna/LisaannaApplication.java
@@ -0,0 +1,13 @@
+package org.example.lisaanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisaannaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisaannaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/lisaanna/LisaannaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lisaanna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisaannaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
