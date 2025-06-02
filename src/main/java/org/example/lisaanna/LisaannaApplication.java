2940992490abe0f5cd1545b8113d1a97cf10aae8
Anna Wallström <anoli89@hotmail.com>
2025-06-02 14:27:24 +0200

    Add branches

--- src/main/java/org/example/lisaanna/LisaannaApplication.java
+++ src/main/java/org/example/lisaanna/LisaannaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LisaannaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LisaannaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LisaannaApplication.class, args);
+    }
 
 }
